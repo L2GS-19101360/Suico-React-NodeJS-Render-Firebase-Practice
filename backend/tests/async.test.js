799e4d96e806bae1943a64b1f80bfe564efe43db
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-11 17:45:28 +0800

    Backend Jest Updated

--- backend/tests/async.test.js
+++ backend/tests/async.test.js
@@ -1,9 +1,20 @@
 const axios = require('axios');
-const jest = require('jest');
-const fetchUser = require('../functions/async')
+const getUserById = require('../functions/async');
 
 jest.mock('axios');
 
-describe('Testing User API with Jest', () => {
-    
+describe('Testing getUserById function', () => {
+    test('fetches user data by document id from the backend server', async () => {
+        const userId = "EZEbWOLpKjOrHQsnmSjD";
+        const userData = { firstname: "Homer" };
+
+        axios.get.mockResolvedValue({ data: userData }); 
+
+        const result = await getUserById(userId); 
+
+        expect(result).toEqual(userData); 
+        expect(axios.get).toHaveBeenCalledWith(
+            `https://suico-react-nodejs-render-firebase-hj4t.onrender.com/api/users/${userId}`
+        ); 
+    });
 });
