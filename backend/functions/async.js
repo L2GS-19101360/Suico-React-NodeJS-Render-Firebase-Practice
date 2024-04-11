518f8e9d9d3b98f176a0303dde5cd376c9abe857
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-12 00:27:42 +0800

    Backend Questionable Updated

--- backend/functions/async.js
+++ backend/functions/async.js
@@ -1,12 +1,14 @@
 const axios = require('axios');
 
-async function getUserById (userId) {
+async function getUserById(userId) {
+    if (typeof userId !== 'string' || !isNaN(Number(userId))) {
+        throw new Error("Invalid User ID");
+    }
 
     const response = await axios.get(
         `https://suico-react-nodejs-render-firebase-hj4t.onrender.com/api/users/${userId}`
     );
-    return response.data
-
+    return response.data;
 }
 
 module.exports = getUserById
--- backend/tests/async.test.js
+++ backend/tests/async.test.js
@@ -4,17 +4,59 @@ const getUserById = require('../functions/async');
 jest.mock('axios');
 
 describe('Testing getUserById function', () => {
-    test('fetches user data by document id from the backend server', async () => {
+    test('fetches user data by document id using string from the backend server', async () => {
         const userId = "EZEbWOLpKjOrHQsnmSjD";
         const userData = { firstname: "Homer" };
 
-        axios.get.mockResolvedValue({ data: userData }); 
+        axios.get.mockResolvedValue({ data: userData });
 
-        const result = await getUserById(userId); 
+        const result = await getUserById(userId);
 
-        expect(result).toEqual(userData); 
+        expect(result).toEqual(userData);
         expect(axios.get).toHaveBeenCalledWith(
             `https://suico-react-nodejs-render-firebase-hj4t.onrender.com/api/users/${userId}`
-        ); 
+        );
+    });
+
+    test('fetches user data by wrong document id from the backend server', async () => {
+        const userId = "joGIy0gWbCvszsAG2zLV";
+        const userData = { firstname: "Homer" };
+
+        axios.get.mockResolvedValue({ data: userData });
+
+        const result = await getUserById(userId);
+
+        expect(result).toEqual(userData);
+        expect(axios.get).toHaveBeenCalledWith(
+            `https://suico-react-nodejs-render-firebase-hj4t.onrender.com/api/users/${userId}`
+        );
+    });
+
+    test('fetches user data by document id but wrong data from the backend server', async () => {
+        const userId = "EZEbWOLpKjOrHQsnmSjD";
+        const userData = { firstname: "Peter" };
+
+        axios.get.mockResolvedValue({ data: userData });
+
+        const result = await getUserById(userId);
+
+        expect(result).toEqual(userData);
+        expect(axios.get).toHaveBeenCalledWith(
+            `https://suico-react-nodejs-render-firebase-hj4t.onrender.com/api/users/${userId}`
+        );
+    });
+
+    test('fetches user data by document id using number from the backend server', async () => {
+        const userId = 1;
+        const userData = { firstname: "Homer" };
+
+        await expect(getUserById(userId)).rejects.toThrow("Invalid User ID");
+    });
+
+    test('fetches user data by document id using character from the backend server', async () => {
+        const userId = '1';
+        const userData = { firstname: "Homer" };
+
+        await expect(getUserById(userId)).rejects.toThrow("Invalid User ID");
     });
 });
